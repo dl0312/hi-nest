7b2a3a6db257801a00c3fbd8e2f636c80e9d96bd
geon lee <geon.lee@navercorp.com>
2020-12-26 22:25:15 +0900

    ✨ movie controller example

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get("search")
+  search(@Query("year") searchingYear: string){
+      return `We are searching for a movie make after ${searchingYear}`
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
+  }
+}
