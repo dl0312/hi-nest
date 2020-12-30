9815122029c1bd1bf6b0ee0a427e7c503288afa9
geon lee <geon.lee@navercorp.com>
2020-12-30 17:21:19 +0900

    ✨ Testing PATCH and DELETE movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,26 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "./../src/app.module";
 
 describe("AppController (e2e)", () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    /** e2e 테스트에서도 main.ts에 설정해놓은 pipe를 적용해야합니다. */
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      })
+    );
     await app.init();
   });
 
@@ -28,7 +37,7 @@ describe("AppController (e2e)", () => {
     it("GET", () => {
       return request(app.getHttpServer()).get("/movies").expect(200).expect([]);
     });
-    it("POST", () => {
+    it("POST 201", () => {
       return request(app.getHttpServer())
         .post("/movies")
         .send({
@@ -38,8 +47,39 @@ describe("AppController (e2e)", () => {
         })
         .expect(201);
     });
+    it("POST 400", () => {
+      return request(app.getHttpServer())
+        .post("/movies")
+        .send({
+          title: "Test",
+          year: 2000,
+          genres: ["test"],
+          other: "thing",
+        })
+        .expect(400);
+    });
     it("DELETE", () => {
       return request(app.getHttpServer()).delete("/movies").expect(404);
+    });
+  });
+
+  describe("/movies/:id", () => {
+    it("GET 200", () => {
+      return request(app.getHttpServer()).get("/movies/1").expect(200);
+    });
+    it("GET 404", () => {
+      return request(app.getHttpServer()).get("/movies/999").expect(404);
+    });
+    it("PATCH 200", () => {
+      return request(app.getHttpServer())
+        .patch("/movies/1")
+        .send({
+          title: "Updated Test",
+        })
+        .expect(200);
+    });
+    it("DELETE 200", () => {
+      return request(app.getHttpServer()).delete("/movies/1").expect(200);
     });
   });
 });
